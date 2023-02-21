a72d34a8b3166df272576ac29258650b672b7b39
Denny Wibowo <indocoffee@gmail.com>
2023-02-21 18:13:08 -0500

    cleanup

--- src/main/java/com/jobapplicationapp/userapi/controller/UserController.java
+++ src/main/java/com/jobapplicationapp/userapi/controller/UserController.java
@@ -3,9 +3,7 @@ package com.jobapplicationapp.userapi.controller;
 import com.jobapplicationapp.userapi.dto.UserRequest;
 import com.jobapplicationapp.userapi.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
